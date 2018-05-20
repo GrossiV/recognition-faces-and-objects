ac444d1f1afb05e0f4bf8be6a1faa5a4436638f7
Guilherme Bezerra <gbsantos.it@gmail.com>
2018-05-20 14:25:40 -0300

    code using other code using another HOG approach

---
+++ face-detection-hog2.py
@@ -0,0 +1,25 @@
+import cv2
+import dlib
+
+subdetector = ["Looking Forward", "Left view", "Right view",
+        "The front turning the left", "The front turning right"]
+
+image = cv2.imread("photos/group.0.jpg")
+detector = dlib.get_frontal_face_detector()
+
+faceDetected, score, idx = detector.run(image)
+
+print(faceDetected)
+print(score)
+print(idx)
+
+for i, face in enumerate(faceDetected):
+    print(i)
+    print(face)
+    print("Detection: {}, score: {}, Sub-detector: {}".format(face, score[i], subdetector[idx[i]]))
+    l, t, r, b = (int(face.left()), int(face.top()), int(face.right()), int(face.bottom()))
+    cv2.rectangle(image, (l, t), (r, b), (0, 0, 255), 2)
+
+cv2.imshow("Detector HOG", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
