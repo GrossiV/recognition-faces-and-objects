1e2f912ca12aa3ace26f1204b20ebe6661001c00
Guilherme Bezerra <gbsantos.it@gmail.com>
2018-05-27 14:22:11 -0300

    Add imglab for recognition of objects and images for tests

---
+++ detector-clock-training.py
@@ -0,0 +1,9 @@
+import dlib
+
+options = dlib.simple_object_detector_training_options()
+options.add_left_right_image_flips = True
+options.C = 5
+
+dlib.train_simple_object_detector("assets/treinamento_relogios.xml", "assets/detector_relogios.svm", options)
+
+
